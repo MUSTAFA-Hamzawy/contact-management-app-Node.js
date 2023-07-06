e48c379c86d1e28e74c9e543186b9cdfbdefc8d9
MUSTAFA Hamzawy <hamzawy2019579@gmail.com>
2023-07-06 20:27:59 +0200

    Created Middleware for handling the thrown errors

---
+++ middlewares/ErrorHandlerMiddleware.js
@@ -0,0 +1,47 @@
+const status = require("../config/status_codes");
+
+const ErrorHandlerMiddleware = (err, req, res, next) => {
+    console.log('Error handlerrrrrrrrrrrrrrr.\n');
+    const statusCode = res.statusCode || status.SERVER_ERROR;
+    let error = "Server Error";
+
+    switch (statusCode) {
+
+        case status.SERVER_ERROR:
+            error = "Server Error.";
+            break;
+        
+        case status.VALIDATION_ERROR:
+            error = "Validation Error.";
+            break;
+                    
+        case status.UNAUTHORIZED:
+            error = "Not Authorized.";
+            break;
+                    
+        case status.NOT_FOUND:
+            error = "404 Not Found.";
+            break;
+                    
+        case status.FORBIDDEN:
+            error = "Forbiddend.";
+            break;
+    
+        default:
+            // No error
+            break;
+    }
+
+    res.json(getErrorBody(error, err.message));
+}
+
+
+const getErrorBody = (err, msg) =>{
+    return {
+        error: err,
+        message : msg
+    }
+}
+
+
+module.exports = ErrorHandlerMiddleware;
