6b4d789cad149c9dc99abed76916462c31538f9f
MUSTAFA Hamzawy <hamzawy2019579@gmail.com>
2023-07-06 20:27:27 +0200

    Created file for status codes

---
+++ config/status_codes.js
@@ -0,0 +1,13 @@
+const status = {
+    DEFAULT : 200,
+    CREATED : 201,
+    UPDATED : 301,
+    REMOVED : 200,
+    VALIDATION_ERROR : 400,
+    UNAUTHORIZED : 401,
+    FORBIDDEN : 403,
+    NOT_FOUND : 404,
+    SERVER_ERROR : 500 
+}
+
+module.exports = status;
