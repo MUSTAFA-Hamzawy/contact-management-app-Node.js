31560ba8ffa55ff92f922e682eb6970b413fdd87
MUSTAFA Hamzawy <hamzawy2019579@gmail.com>
2023-07-06 20:28:12 +0200

    Update server.js

--- server.js
+++ server.js
@@ -1,7 +1,24 @@
 //  some configs
 const {app, express} = require('./config/app');
+const ErrorHandlerMiddleware = require('./middlewares/ErrorHandlerMiddleware');
 require('dotenv').config();
 const PORT = process.env.PORT || 8080;
+const contactRoutes = require('./routes/contact');
+
+
+
+
+app.use(express.json()); // Built-in Middleware To parse any request body
+
+
+
+
+/***************************   Routes  *************************************/
+
+app.use('/contacts', contactRoutes);
+
+/***************************   Middlewares  *************************************/
+app.use(ErrorHandlerMiddleware);
 
 
 // Listening for any request
