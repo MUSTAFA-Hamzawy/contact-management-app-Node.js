835316091227fb4f9488d76e847cfc5373c2e672
MUSTAFA Hamzawy <hamzawy2019579@gmail.com>
2023-07-07 08:11:25 +0200

    Added the db connection

--- server.js
+++ server.js
@@ -1,17 +1,28 @@
 //  some configs
 const {app, express} = require('./config/app');
+const connectDB = require('./config/dbConnection');
+const mongoose = require('mongoose');
 const ErrorHandlerMiddleware = require('./middlewares/ErrorHandlerMiddleware');
 require('dotenv').config();
 const PORT = process.env.PORT || 8080;
 const contactRoutes = require('./routes/contact');
 
 
+// db connection
+connectDB();
+
+mongoose.connection.once('open', ()=>{
+    console.log('connected to db.');
+
+    // Listening for any request, if connected to db successfully
+    app.listen(PORT, ()=>{
+        console.log(`Server is running on port : ${PORT}`);
+    })
+
+})
 
 
 app.use(express.json()); // Built-in Middleware To parse any request body
-
-
-
 
 /***************************   Routes  *************************************/
 
@@ -19,9 +30,3 @@ app.use('/contacts', contactRoutes);
 
 /***************************   Middlewares  *************************************/
 app.use(ErrorHandlerMiddleware);
-
-
-// Listening for any request
-app.listen(PORT, ()=>{
-    console.log(`Server is running on port : ${PORT}`);
-})
