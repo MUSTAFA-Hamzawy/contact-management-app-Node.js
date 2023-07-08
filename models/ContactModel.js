941bcb8cc04f5dfe5268c2cd97544d8c5dbad79e
MUSTAFA Hamzawy <hamzawy2019579@gmail.com>
2023-07-08 17:58:25 +0200

    Created the model/shcema for the Contacts

---
+++ models/ContactModel.js
@@ -0,0 +1,24 @@
+const mongoose = require('mongoose');
+const schema = mongoose.Schema;
+
+const ContactSchema = new schema(
+    {
+        fullName: {
+            type: String,
+            required: [true, "Please enter your full name."]
+        },
+        email: {
+            type: String,
+            required: [true, "Email is required."]
+        },
+        phoneNumber: {
+            type: String
+        }
+    },
+    {
+        timestamps: true
+    }
+);
+
+
+module.exports = mongoose.model('ContactModel', ContactSchema);
