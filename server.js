f1dde348d236e602b6b038f7c50b19ba4f1bb66f
MUSTAFA Hamzawy <hamzawy2019579@gmail.com>
2023-07-08 18:35:20 +0200

    Added routes for the user auth

--- server.js
+++ server.js
@@ -1,32 +1,35 @@
 //  some configs
-const {app, express} = require('./config/app');
+const express = require('express');
+const app = express();
 const connectDB = require('./config/dbConnection');
 const mongoose = require('mongoose');
 const ErrorHandlerMiddleware = require('./middlewares/ErrorHandlerMiddleware');
 require('dotenv').config();
 const PORT = process.env.PORT || 8080;
 const contactRoutes = require('./routes/contact');
+const userRoutes = require('./routes/user');
 
 
 // db connection
-connectDB();
+// connectDB();
 
-mongoose.connection.once('open', ()=>{
-    console.log('connected to db.');
+// mongoose.connection.once('open', ()=>{
+//     console.log('connected to db.');
 
     // Listening for any request, if connected to db successfully
     app.listen(PORT, ()=>{
         console.log(`Server is running on port : ${PORT}`);
     })
 
-})
+// })
 
 
 app.use(express.json()); // Built-in Middleware To parse any request body
 
 /***************************   Routes  *************************************/
-
+app.use('/user', userRoutes)
 app.use('/contacts', contactRoutes);
+
 
 /***************************   Middlewares  *************************************/
 app.use(ErrorHandlerMiddleware);
