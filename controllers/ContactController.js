24ec13d4b438b00a6a4df812f2784902bbf4ed05
MUSTAFA Hamzawy <hamzawy2019579@gmail.com>
2023-07-09 20:35:20 +0200

    Protected the CRUD for the contact
    Only Authorized users can make CRUD

--- controllers/ContactController.js
+++ controllers/ContactController.js
@@ -3,14 +3,20 @@ const asyncHandler = require('express-async-handler');
 const ContactModel = require('../models/ContactModel');
 
 const getContacts = asyncHandler( async (req, res) => {
-        const data = await ContactModel.find();
+        const data = await ContactModel.find({user_id: req.user.id});
         res.status(status.DEFAULT).json(data);
 })
 
 const getContact = asyncHandler( async (req, res) =>  {
         const id = req.params.id;
+        const currentUserId = req.user.id;
+        const data = await ContactModel.findOne({id}) || {};
 
-        const data = await ContactModel.findById(id) || {};
+        if(data.user_id.toString() != currentUserId){
+                res.status(status.UNAUTHORIZED);
+                throw new Error("User is not authorized to fetch this contact.")
+        }
+
         if(data) res.status(status.DEFAULT).json(data);
         else {
                 res.status(status.NOT_FOUND);
@@ -20,19 +26,29 @@ const getContact = asyncHandler( async (req, res) =>  {
 
 const createContact = asyncHandler( async (req, res) =>  {
     const {fullName, email, phoneNumber} = req.body;
+    const currentUserId = req.user.id; 
 
     // Logic
-    const data = await ContactModel.create({fullName, email, phoneNumber});
+    const data = await ContactModel.create({fullName, email, phoneNumber, user_id: currentUserId});
     return res.status(status.CREATED).json(data);
 })
 
 const updateContact = asyncHandler( async (req, res) =>  {
         const id = req.params.id;
-        const contact = ContactModel.findById(id);
+        const currentUserId = req.user.id;
+        const contact = await ContactModel.findOne({id});
+
         if(!contact){
                 res.status(status.NOT_FOUND);
                 throw new Error("Not found contact.")
         }
+        
+        if(contact.user_id.toString() != currentUserId){
+                res.status(status.UNAUTHORIZED);
+                throw new Error("User is not authorized to update this contact.")
+        }
+
+
         const updatedData = await ContactModel.findByIdAndUpdate(
                 id,
                 req.body,
@@ -43,9 +59,16 @@ const updateContact = asyncHandler( async (req, res) =>  {
 })
 const deleteContact = asyncHandler( async (req, res) =>{ 
         const id = req.params.id;
-        if(! ContactModel.findById(id)){
+        const currentUserId = req.user.id;
+        const contact = await ContactModel.findOne({id});
+        if(! contact){
                 res.status(status.NOT_FOUND);
                 throw new Error("Not found contact.")
+        }
+
+        if(contact.user_id.toString() != currentUserId){
+                res.status(status.UNAUTHORIZED);
+                throw new Error("User is not authorized to delete this contact.")
         }
 
         const result = await ContactModel.findByIdAndRemove(id);
